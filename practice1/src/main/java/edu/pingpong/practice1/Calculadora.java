837045b074126974578ad69cfed33d93a3e94754
Ian Mercadal <72464723+IanMercadal@users.noreply.github.com>
2021-05-13 20:15:29 +0200

    feat: Finished all operations

--- practice1/src/main/java/edu/pingpong/practice1/Calculadora.java
+++ practice1/src/main/java/edu/pingpong/practice1/Calculadora.java
@@ -1,5 +1,6 @@
 package edu.pingpong.practice1;
-
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Calculadora {
 
@@ -9,6 +10,12 @@ public class Calculadora {
 
     public static void operaciones(){
     
+        sumar sumar = new sumar();
+        restar restar = new restar();
+        multiplicar multiplicar = new multiplicar();
+        dividir dividir = new dividir();
+        resto resto = new resto();
+
         Scanner teclado = new Scanner(System.in);
         boolean salir = false;
         int opcion;
@@ -19,7 +26,8 @@ public class Calculadora {
             System.out.println("2. Opcion 2 - Restar.");
             System.out.println("3. Opcion 3 - Multiplicar.");
             System.out.println("4. Opcion 4 - Dividir.");
-            System.out.println("5. Opcion 5 - Salir.");
+            System.out.println("5. Opcion 5 - Resto division.");
+            System.out.println("6. Opcion 6 - Salir.");
  
             try {
  
@@ -29,26 +37,31 @@ public class Calculadora {
                 switch (opcion) {
                     case 1:
                         System.out.println("Has seleccionado la opcion 1");
-                        //fiveOrderedNumbers.five();
+                        sumar.suma();
                         break;
                     case 2:
                         System.out.println("Has seleccionado la opcion 2");
-                        //fiveInverseNumbers.fiveInverse();
+                        restar.resta();
                         break;
                     case 3:
                         System.out.println("Has seleccionado la opcion 3");
-                        //numbersPositiveNegatives.posNeg();
+                        multiplicar.operacionMultiplicar();
                         break;
                     case 4:
                         System.out.println("Has seleccionado la opcion 4");
-                        //stringLength.stringInputLenght();
+                        dividir.operacionDividir();
                         break;
                     case 5:
+                        System.out.println("Has seleccionado la opcion 5");
+                        resto.operacionResto();
+                        break;
+                    case 6:
                         salir = true;
+                        System.out.println("Gracias por usar la calculadora, hasta la proxima.");
                         break; 
 
                     default:
-                        System.out.println("Solo nnmeros entre 1 y 5");
+                        System.out.println("Solo numeros entre 1 y 6");
                 }
             } catch (InputMismatchException e) {
                 System.out.println("Debes insertar un numero");
---
+++ practice1/src/main/java/edu/pingpong/practice1/restar.java
@@ -0,0 +1,21 @@
+package edu.pingpong.practice1;
+import java.util.Scanner;
+
+public class restar{
+    public void resta(){
+
+        System.out.println("Introduce 2 numeros");
+
+        System.out.println("Introduce el primer numero");
+        Scanner entrada = new Scanner(System.in);
+        float numero1 = entrada.nextFloat();
+
+        System.out.println("Introduce el segundo numero");
+        Scanner entrada2 = new Scanner(System.in);
+        float numero2 = entrada.nextFloat();
+    
+        float sumaTotal = numero1 - numero2;
+
+        System.out.println("La resta es: " + sumaTotal);
+    }
+}
